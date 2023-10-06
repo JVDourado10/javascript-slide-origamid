c185c6970956528d01e323455546142164e515a5
JVDourado10 <jovneiva@gmail.com>
2023-10-06 14:52:09 -0300

    add: paginação por slide

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const slide = new SlideNav('.slide', '.slide-wrapper');
 
 slide.init()
 slide.addArrow('.prev', '.next')
+slide.addControl()
+
 
 // slide.changeSlide(1);
 // slide.activePrevSlide();
