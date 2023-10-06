87cc2d5194e55b15a0decd791e7493a16b87a23e
JVDourado10 <jovneiva@gmail.com>
2023-10-06 15:05:15 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-import {SlideNav} from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 
 slide.init()
 slide.addArrow('.prev', '.next')
-slide.addControl()
-
-
-// slide.changeSlide(1);
-// slide.activePrevSlide();
+slide.addControl('.custom-controls')
